ec49a3900215ff9645d993966ff940efcdb0bc08
Precious Nobonga <precious.nobonga@gmail.com>
2024-08-13 14:22:09 +0200

    Update Program.cs
    Modified Program.cs to support CORS and IIS integration

--- EmployeeDemographics/EmployeeDemographics/Program.cs
+++ EmployeeDemographics/EmployeeDemographics/Program.cs
@@ -1,12 +1,25 @@
+using EmployeeDemographics.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
