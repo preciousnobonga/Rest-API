072d30ce9131a823bb03f54f03d7c4c55178b639
Precious Nobonga <precious.nobonga@gmail.com>
2024-08-13 13:55:10 +0200

    Update ServiceExtensions.cs
    - Instead of the AllowAnyOrigin() method which allows requests from any source, we can use the WithOrigins("https://example.com") which will allow requests only from that concrete source.
    - Instead of AllowAnyMethod() that allows HTTP methods, we can use WithMethods("POST", "GET") that will allow only specific HTTP methods.
    - Instead of AllowAnyHeader() method, we can use WithHeaders("accept", "content-type") method to allow only specific headers.

--- EmployeeDemographics/EmployeeDemographics/Extensions/ServiceExtensions.cs
+++ EmployeeDemographics/EmployeeDemographics/Extensions/ServiceExtensions.cs
@@ -2,5 +2,17 @@
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.WithOrigins()
+                           .WithMethods("POST", "GET")
+                           .WithHeaders("accept", "content-type");
+                });
+            });
+        }
     }
 }
