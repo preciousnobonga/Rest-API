3c334321782b4b72b57349252e73e23edb68dde8
Precious Nobonga <precious.nobonga@gmail.com>
2024-09-03 10:13:41 +0200

    Update PokemonReviewApp.AssemblyInfo.cs

--- PokemonReviewApp/PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7852534e6fcda1cfa68c8bc30d1d6e66f5a14d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7963033143b9551a1b4205f4831d1fbfce8c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
