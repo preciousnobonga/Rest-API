a95e5df62f910e95ba23f6a9fed464f6e488180d
Precious Nobonga <precious.nobonga@gmail.com>
2024-09-03 13:07:15 +0200

    Update Pokemon.cs

--- PokemonReviewApp/PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/PokemonReviewApp/Models/Pokemon.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; }
 
     }
 }
